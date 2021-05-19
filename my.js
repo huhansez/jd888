d3c715f8ec097fb3f91d376258c3877868aab3cb
huhansez <83069374+huhansez@users.noreply.github.com>
2021-05-19 13:32:39 +0800

    Update my.js

--- my.js
+++ my.js
@@ -1,3 +1,5 @@
 5 * * * * https://raw.githubusercontent.com/huhansez/jd888/main/my.js
-rm -f /jd/jshare.sh
-cp -f /jd/config/jshare.sh /jd/
+fi
+rm -rf /jd/jshare.sh
+cp -rf /jd/config/jshare.sh /jd/
+if
